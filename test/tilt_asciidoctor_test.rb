d03df7b8eeefbd18227429776ca0c2f37299f3be
Jeremy Evans <code@jeremyevans.net>
2023-04-05 12:40:53 -0700

    Increase asciidoctor template coverage

--- test/tilt_asciidoctor_test.rb
+++ test/tilt_asciidoctor_test.rb
@@ -2,7 +2,9 @@ require_relative 'test_helper'
 
 begin
   require 'tilt/asciidoc'
-
+rescue LoadError => e
+  warn "Tilt::AsciidoctorTemplate (disabled): #{e.message}"
+else
   describe 'tilt/asciidoctor' do
     html5_output = "<div class=\"sect1\"><h2 id=\"_hello_world\">Hello World!</h2><div class=\"sectionbody\"></div></div>"
     docbook5_output = "<section xml:id=\"_hello_world\"><title>Hello World!</title></section>"
@@ -32,7 +34,7 @@ begin
 
     it "preparing and evaluating html5 templates on #render" do
       template = Tilt::AsciidoctorTemplate.new(:attributes => {"backend" => 'html5'}) { |t| "== Hello World!" } 
-      assert_equal html5_output, strip_space(template.render)
+      3.times { assert_equal html5_output, strip_space(template.render) }
     end
 
     it "preparing and evaluating docbook 5 templates on #render" do
@@ -40,11 +42,15 @@ begin
       assert_equal docbook5_output, strip_space(template.render)
     end
 
-    it "can be rendered more than once" do
-      template = Tilt::AsciidoctorTemplate.new(:attributes => {"backend" => 'html5'}) { |t| "== Hello World!" } 
-      3.times { assert_equal html5_output, strip_space(template.render) }
+    it "supports header_footer: true option" do
+      template = Tilt::AsciidoctorTemplate.new(:header_footer=>true) { |t| "== Hello World!" }
+      output = strip_space(template.render)
+      assert_includes output, html5_output
+      assert_match(/\A<!DOCTYPE html>.*<\/html>\z/m, output)
+    end
+
+    it "sets allows_script metadata set to false" do
+      assert_equal false, Tilt::AsciidoctorTemplate.new{ |t| "" }.metadata[:allows_script]
     end
   end
-rescue LoadError
-  warn "Tilt::AsciidoctorTemplate (disabled)"
 end

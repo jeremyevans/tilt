77b514f12d91ae956f450b23d562eecd3a7d3b8d
Jeremy Evans <code@jeremyevans.net>
2023-04-10 12:58:21 -0700

    Increase creole template coverage

--- test/tilt_creoletemplate_test.rb
+++ test/tilt_creoletemplate_test.rb
@@ -2,7 +2,9 @@ require_relative 'test_helper'
 
 begin
   require 'tilt/creole'
-
+rescue LoadError => e
+  warn "Tilt::CreoleTemplate (disabled): #{e.message}"
+else
   describe 'tilt/creole' do
     it "is registered for '.creole' files" do
       assert_equal Tilt::CreoleTemplate, Tilt['test.creole']
@@ -10,14 +12,16 @@ begin
 
     it "compiles and evaluates the template on #render" do
       template = Tilt::CreoleTemplate.new { |t| "= Hello World!" }
-      assert_equal "<h1>Hello World!</h1>", template.render
-    end
-
-    it "can be rendered more than once" do
-      template = Tilt::CreoleTemplate.new { |t| "= Hello World!" }
       3.times { assert_equal "<h1>Hello World!</h1>", template.render }
     end
+
+    it "supports :allowed_schemes, :extensions, :no_escape options" do
+      template = Tilt::CreoleTemplate.new(:allowed_schemes=>['http'], :extensions=>true, :no_escape=>true) { |t| "[[/Test]]" }
+      assert_equal "<p><a href=\"/Test\">/Test</a></p>", template.render
+    end
+
+    it "sets allows_script metadata set to false" do
+      assert_equal false, Tilt::CreoleTemplate.new { |t| "= Hello World!" }.metadata[:allows_script]
+    end
   end
-rescue LoadError
-  warn "Tilt::CreoleTemplate (disabled)"
 end

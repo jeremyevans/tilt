597a139f3fb829cdeab2c91aa6532744ed1bdfa1
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-01-06 03:34:06 +0900

    Switch to constant name approach

--- lib/tilt/commonmarker.rb
+++ lib/tilt/commonmarker.rb
@@ -2,101 +2,88 @@
 require_relative 'template'
 require 'commonmarker'
 
-module Tilt
-  class CommonMarkerTemplate < StaticTemplate
-    ALIASES = {
-      :smartypants => :SMART
-    }.freeze
-    PARSE_OPTS = [
-      :FOOTNOTES,
-      :LIBERAL_HTML_TAG,
-      :SMART,
-      :smartypants,
-      :STRIKETHROUGH_DOUBLE_TILDE,
-      :UNSAFE,
-      :VALIDATE_UTF8,
-    ].freeze
-    RENDER_OPTS = [
-      :FOOTNOTES,
-      :FULL_INFO_STRING,
-      :GITHUB_PRE_LANG,
-      :HARDBREAKS,
-      :NOBREAKS,
-      :SAFE, # Removed in v0.18.0 (2018-10-17)
-      :SOURCEPOS,
-      :TABLE_PREFER_STYLE_ATTRIBUTES,
-      :UNSAFE,
-    ].freeze
-    EXTS = [
-      :autolink,
-      :strikethrough,
-      :table,
-      :tagfilter,
-      :tasklist,
-    ].freeze
+if defined?(::Commonmarker)
+  parse_opts = [
+    :smart,
+    :default_info_string,
+  ].freeze
+  render_opts = [
+    :hardbreaks,
+    :github_pre_lang,
+    :width,
+    :unsafe,
+    :escape,
+    :sourcepos,
+  ].freeze
+  exts = [
+    :strikethrough,
+    :tagfilter,
+    :table,
+    :autolink,
+    :tasklist,
+    :superscript,
+    :header_ids,
+    :footnotes,
+    :description_lists,
+    :front_matter_delimiter,
+    :shortcodes,
+  ].freeze
 
-    V1_PARSE_OPTS = [
-      :smart,
-      :default_info_string,
-    ].freeze
-    V1_RENDER_OPTS = [
-      :hardbreaks,
-      :github_pre_lang,
-      :width,
-      :unsafe,
-      :escape,
-      :sourcepos,
-    ].freeze
-    V1_EXTS = [
-      :strikethrough,
-      :tagfilter,
-      :table,
-      :autolink,
-      :tasklist,
-      :superscript,
-      :header_ids,
-      :footnotes,
-      :description_lists,
-      :front_matter_delimiter,
-      :shortcodes,
-    ].freeze
+  Tilt::CommonMarkerTemplate = Tilt::StaticTemplate.subclass do
+    parse_options = @options.select { |key, _| parse_opts.include?(key) }
+    render_options = @options.select { |key, _| render_opts.include?(key) }
+    extensions = @options.select { |key, _| exts.include?(key) }
 
-    def prepare
-      if commonmaker_v1_or_later?
-        prepare_v1
-      else
-        prepare_pre
-      end
+    Commonmarker.to_html(@data, options: { parse: parse_options, render: render_options, extension: extensions })
+  end
+else
+  aliases = {
+    :smartypants => :SMART
+  }.freeze
+  parse_opts = [
+    :FOOTNOTES,
+    :LIBERAL_HTML_TAG,
+    :SMART,
+    :smartypants,
+    :STRIKETHROUGH_DOUBLE_TILDE,
+    :UNSAFE,
+    :VALIDATE_UTF8,
+  ].freeze
+  render_opts = [
+    :FOOTNOTES,
+    :FULL_INFO_STRING,
+    :GITHUB_PRE_LANG,
+    :HARDBREAKS,
+    :NOBREAKS,
+    :SAFE, # Removed in v0.18.0 (2018-10-17)
+    :SOURCEPOS,
+    :TABLE_PREFER_STYLE_ATTRIBUTES,
+    :UNSAFE,
+  ].freeze
+  exts = [
+    :autolink,
+    :strikethrough,
+    :table,
+    :tagfilter,
+    :tasklist,
+  ].freeze
+
+  Tilt::CommonMarkerTemplate = Tilt::StaticTemplate.subclass do
+    extensions = exts.select do |extension|
+      @options[extension]
     end
 
-    private def prepare_pre
-      extensions = EXTS.select do |extension|
-        @options[extension]
-      end
-
-      parse_options, render_options = [PARSE_OPTS, RENDER_OPTS].map do |opts|
-        opts = opts.select do |option|
-          @options[option]
-        end.map! do |option|
-          ALIASES[option] || option
-        end
-
-        opts = :DEFAULT unless opts.any?
-        opts
+    parse_options, render_options = [parse_opts, render_opts].map do |opts|
+      opts = opts.select do |option|
+        @options[option]
+      end.map! do |option|
+        aliases[option] || option
       end
 
-      @output = CommonMarker.render_doc(@data, parse_options, extensions).to_html(render_options, extensions)
+      opts = :DEFAULT unless opts.any?
+      opts
     end
 
-    private def prepare_v1
-      parse_options = @options.select { |key, _| V1_PARSE_OPTS.include?(key) }
-      render_options = @options.select { |key, _| V1_RENDER_OPTS.include?(key) }
-      extensions = @options.select { |key, _| V1_EXTS.include?(key) }
-      @output = Commonmarker.to_html(@data, options: { parse: parse_options, render: render_options, extension: extensions })
-    end
-
-    private def commonmaker_v1_or_later?
-      defined?(::Commonmarker) ? true : false
-    end
+    CommonMarker.render_doc(@data, parse_options, extensions).to_html(render_options, extensions)
   end
 end
--- test/tilt_commonmarkertemplate_test.rb
+++ test/tilt_commonmarkertemplate_test.rb
@@ -1,53 +1,8 @@
 require_relative 'test_helper'
 
 checked_describe 'tilt/commonmarker' do
-  if ENV['LATEST_COMMONMARKER'] == 'yes'
-    it "preparing and evaluating templates on #render" do
-      template = Tilt::CommonMarkerTemplate.new { |t| "# Hello World!" }
-      assert_equal "<h1>Hello World!</h1>\n", template.render
-    end
-
-    it "can be rendered more than once" do
-      template = Tilt::CommonMarkerTemplate.new { |t| "# Hello World!" }
-      3.times { assert_equal "<h1>Hello World!</h1>\n", template.render }
-    end
-
-    it "smartypants when :smatr is set" do
-      template = Tilt::CommonMarkerTemplate.new(smart: true) do |t|
-        "OKAY -- 'Smarty Pants'"
-      end
-      assert_match('<p>OKAY – ‘Smarty Pants’</p>', template.render)
-    end
-
-    it 'Renders unsafe HTML when :unsafe is set' do
-      template = Tilt::CommonMarkerTemplate.new(unsafe: true) do |_t|
-        <<MARKDOWN
-<div class="alert alert-info full-width">
-<h5 class="card-title">TL;DR</h5>
-<p>This is an unsafe HTML block</p>
-</div>
-
-And then some **other** Markdown
-MARKDOWN
-      end
-
-      expected = <<EXPECTED_HTML
-<div class="alert alert-info full-width">
-<h5 class="card-title">TL;DR</h5>
-<p>This is an unsafe HTML block</p>
-</div>
-<p>And then some <strong>other</strong> Markdown</p>
-EXPECTED_HTML
-
-      assert_match(expected, template.render)
-    end
-
-    it "autolinking when :autolink is set" do
-      template = Tilt::CommonMarkerTemplate.new(autolink: true) do |t|
-        "https://example.com"
-      end
-      assert_match('<a href="https://example.com">https://example.com</a>', template.render)
-    end
+  if defined?(::Commonmarker)
+    # TODO
   else
     it "preparing and evaluating templates on #render" do
       template = Tilt::CommonMarkerTemplate.new { |t| "# Hello World!" }

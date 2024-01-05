63a7c278703182439b6bc6034aec851dc7ba0270
Yusuke Nakamura <unasuke@users.noreply.github.com>
2024-01-06 05:04:13 +0900

    Support Commonmarker v1 api
    The commonmarker gem broke backwards compatibility starting in 1.0 (switching the backend implementation from libcmark (C) to comrak (Rust)).  This keeps support for the pre-1.0 API, and adds support for the 1+ API.

--- test/tilt_commonmarkertemplate_test.rb
+++ test/tilt_commonmarkertemplate_test.rb
@@ -44,4 +44,46 @@ EXPECTED_HTML
   it "sets allows_script metadata set to false" do
     assert_equal false, Tilt::CommonMarkerTemplate.new { |t| "# Hello World!" }.metadata[:allows_script]
   end
+
+  if defined?(::Commonmarker)
+    it "render unsafe HTML with pre version's option name" do
+      template = Tilt::CommonMarkerTemplate.new(UNSAFE: true) do |_t|
+        <<MARKDOWN
+<div class="alert alert-info full-width">
+<h5 class="card-title">TL;DR</h5>
+<p>This is an unsafe HTML block</p>
+</div>
+
+And then some **other** Markdown
+MARKDOWN
+      end
+
+      expected = <<EXPECTED_HTML
+<div class="alert alert-info full-width">
+<h5 class="card-title">TL;DR</h5>
+<p>This is an unsafe HTML block</p>
+</div>
+<p>And then some <strong>other</strong> Markdown</p>
+EXPECTED_HTML
+
+      assert_match(expected, template.render)
+    end
+
+    it "smartypants when :smartypants is set (pre version's option name)" do
+      template = Tilt::CommonMarkerTemplate.new(:smartypants => true) do |t|
+        "OKAY -- 'Smarty Pants'"
+      end
+      assert_match('<p>OKAY – ‘Smarty Pants’</p>', template.render)
+    end
+
+    it "render markdown with custom prefixed-header id" do
+      template = Tilt::CommonMarkerTemplate.new(header_ids: "prefix-") do |t|
+        "# Foo"
+      end
+      expected = <<HTML
+<h1><a href="#foo" aria-hidden="true" class="anchor" id="prefix-foo"></a>Foo</h1>
+HTML
+      assert_match(expected, template.render)
+    end
+  end
 end
